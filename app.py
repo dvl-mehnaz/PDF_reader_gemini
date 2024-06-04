4f47b6a3cbc887308b284d5b8259d9fa0de5b965
mehnaz02 <mehnazkhan.kpg@gmail.com>
2024-06-04 16:00:09 +0530

    final

--- app.py
+++ app.py
@@ -63,13 +63,10 @@ def get_conversational_chain():
 
 
 def user_input(user_question):
-    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
-    
-    new_db = FAISS.load_local("faiss_index", embeddings)
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", k =0.3)
+    new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
     docs = new_db.similarity_search(user_question)
-
     chain = get_conversational_chain()
-
     
     response = chain(
         {"input_documents":docs, "question": user_question}
